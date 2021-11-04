84bd65a6b99a541d095b0620a6ce7d9a4163e9bd
dabes1710 <david.belmar@mayor.cl>
2021-11-03 23:09:10 -0300

    migraciones

--- db/migrate/20211103231308_create_products.rb
+++ db/migrate/20211103231308_create_products.rb
@@ -4,7 +4,7 @@ class CreateProducts < ActiveRecord::Migration[6.1]
       t.string :name
       t.integer :price
       t.string :description
-      t.references :category_id, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
